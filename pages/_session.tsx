6d19966080eab84613d35f24350a9a09959055e5
Olumayowa Olowomeye <52811635+Mav-ert@users.noreply.github.com>
2022-02-23 10:48:43 -0600

    added socket base

--- pages/_session.tsx
+++ pages/_session.tsx
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React, { Component,useState, useEffect } from "react";
 import {Button, Page, Text, GeistProvider, CssBaseline } from '@geist-ui/core'
-const Session = () => (
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:5000";
+function Session() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("/queuejoin", data => {
+      setResponse(data);
+    });
+  }, []);
+
+  return (
     <Page>
       <Text h1>Session Page</Text>
       <Button>Submit</Button>
+      <p>
+      It's <time dateTime={response}>{response}</time>
+      </p>
     </Page>
-  )
+  );
+}
 
 export default Session
