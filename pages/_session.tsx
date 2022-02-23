4e6ea0908d212901887bb6ca9117f75f47027865
Olumayowa Olowomeye <52811635+Mav-ert@users.noreply.github.com>
2022-02-23 10:02:21 -0600

    added sessions page

---
+++ pages/_session.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {Button, Page, Text, GeistProvider, CssBaseline } from '@geist-ui/core'
+const Session = () => (
+    <Page>
+      <Text h1>Session Page</Text>
+      <Button>Submit</Button>
+    </Page>
+  )
+
+export default Session
