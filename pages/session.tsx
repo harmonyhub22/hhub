c883f91924b9b80a9521b387a0d4393e0c5cf19d
Dean Orenstein <dean.orenstein2@gmail.com>
2022-02-25 15:50:57 -0600

    next web socket code

--- pages/session.tsx
+++ pages/session.tsx
@@ -1,13 +1,34 @@
 import React, { Component,useState, useEffect } from "react";
 import {Button, Page, Text, GeistProvider, CssBaseline, Table } from '@geist-ui/core'
-import socketIOClient from "socket.io-client";
+import io from "socket.io-client";
 const ENDPOINT = "http://localhost:5000";
 
-
 function Session() {
-  
   const [response, setResponse] = useState("");
+  const [input, setInput] = useState("");
+  const [socket, setSocket] = useState({});
 
+  const socketInitializer = async () => {
+    await fetch("/api/socket");
+    const newSocket = io();
+
+    newSocket.on("connect", () => {
+      console.log("connected");
+    });
+
+    newSocket.on("update-input", (msg) => {
+      console.log("input updated!")
+    });
+
+    setSocket(newSocket);
+  };
+
+  useEffect(() => socketInitializer(), []);
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+    socket.emit("input-change", e.target.value);
+  };
 
   const addLayer = () =>{
     alert("added")
@@ -21,10 +42,18 @@ function Session() {
     <Page>
       <Text h1>Session Page</Text>
       <audio controls>
-        <source src = "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_1MG.mp3" type = "audio/mpeg"></source> 
+        <source
+          src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_1MG.mp3"
+          type="audio/mpeg"
+        ></source>
       </audio>
-      <Button onClick = {() => addLayer()}>Submit Layer</Button>
-      <Button onClick = {() => finishSong()}>Finish Song</Button>
+      <Button onClick={() => addLayer()}>Submit Layer</Button>
+      <Button onClick={() => finishSong()}>Finish Song</Button>
+      <input
+        placeholder="Type something"
+        value={input}
+        onChange={onChangeHandler}
+      />
     </Page>
   );
 }
