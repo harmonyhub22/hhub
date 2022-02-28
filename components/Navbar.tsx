4eebd322e158e6a1d39eadacc270d82ea079be29
Olumayowa Olowomeye <52811635+Mav-ert@users.noreply.github.com>
2022-02-28 09:07:08 -0600

    Slight changes
    Changes to fix warnings and some errors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,19 +4,21 @@ import * as AiIcons from 'react-icons/ai';
 import Link from 'next/link';
 import { SidebarData } from './NavbarData';
 import styles from "./Navbar.module.css";
+import { IconContext } from 'react-icons';
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false)
   const showSidebar = () => setSidebar(!sidebar)
   return (
+    <IconContext.Provider value={{ color: 'black' }}>
     <div className={styles.navbar}>
-      <Link href="/" className={styles.menu_bars} passHref>
+      <Link href="/" className={styles.menu_bars}>
         <FaIcons.FaBars onClick={showSidebar} />
       </Link>
       <nav className={sidebar ? styles.nav_menu_active : styles.nav_menu}>
         <ul className={styles.nav_menu_items}>
           <li className={styles.navbar_toggle}>
-            <Link href="/" className={styles.menu_bars} passHref>
+            <Link href="/" className={styles.menu_bars}>
               <AiIcons.AiOutlineClose onClick={showSidebar} />
             </Link>
           </li>
@@ -24,7 +26,7 @@ const Navbar = () => {
             return (
               <li key={index} className={item.cName}>
                 {item.icon}
-                <Link href={item.path} passHref>
+                <Link href={item.path}>
                   <span className={styles.navbar_item_label}>{item.title}</span>
                 </Link>
               </li>
@@ -33,6 +35,7 @@ const Navbar = () => {
         </ul>
       </nav>
     </div>
+    </IconContext.Provider>
   );
 }
 
