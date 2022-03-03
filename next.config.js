cc6dff2fd12e828b1ce0019b6d285afb3552bab2
Gregory Petri <gcpetri@outlook.com>
2022-03-03 16:10:54 -0600

    config updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const defaultServerUrl = 'https://harmony-hub-backend.herokuapp.com/'
+const defaultFallbackUrl = 'https://harmony-hub-backend.herokuapp.com/:path*';
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SERVER_URL: 'http://localhost:5000/api/',
+    SERVER_URL: process.env.SERVER_URL || defaultServerUrl,
   },
+  async rewrites() {
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: '/:path*',
+          basePath: false,
+          destination: process.env.FALLBACK_URL || defaultFallbackUrl
+        }
+      ]
+    };
+  }
 }
 
 module.exports = nextConfig
