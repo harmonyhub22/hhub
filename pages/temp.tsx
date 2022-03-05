eebe045a5a7aaffe95c997afe2e0102a1b6c4216
Dean Orenstein <dean.orenstein2@gmail.com>
2022-03-05 11:26:57 -0600

    merge master and palette branches

--- pages/temp.tsx
+++ pages/temp.tsx
@@ -19,60 +19,267 @@ import { saveAs } from "file-saver";
 import Timeline from "react-calendar-timeline";
 import moment from "moment";
 import Head from "next/head";
+import * as Tone from "tone";
+import { Player } from "tone";
 
 function Session() {
   const [response, setResponse] = useState("");
-//   const [socket, setSocket] = useState(io);
+  //   const [socket, setSocket] = useState(io);
   const { visible, setVisible, bindings } = useModal();
   const [showPallete, setShowPallete] = React.useState(false);
-//   const router = useRouter();
+  //   const router = useRouter();
 
-//   const sessionId = router.query.id;
+  //   const sessionId = router.query.id;
 
-    // groups represent our layers 
-    const groups = [
-      { id: 1, title: "layer 1" },
-      { id: 2, title: "layer 2" },
-    ];
+  // groups represent our layers
+  const groups = [
+    { id: 1, title: "layer 1" },
+    { id: 2, title: "layer 2" },
+  ];
 
-    const items = [
-      {
-        id: 1,
-        group: 1,
-        title: "item 1",
-        start_time: moment(),
-        end_time: moment().add(1, "hour"),
-      },
-      {
-        id: 2,
-        group: 2,
-        title: "item 2",
-        start_time: moment().add(-0.5, "hour"),
-        end_time: moment().add(0.5, "hour"),
-      },
-      {
-        id: 3,
-        group: 1,
-        title: "item 3",
-        start_time: moment().add(2, "hour"),
-        end_time: moment().add(3, "hour"),
-      },
-    ];
+  const items = [
+    {
+      id: 1,
+      group: 1,
+      title: "item 1",
+      start_time: moment(),
+      end_time: moment().add(1, "hour"),
+    },
+    {
+      id: 2,
+      group: 2,
+      title: "item 2",
+      start_time: moment().add(-0.5, "hour"),
+      end_time: moment().add(0.5, "hour"),
+    },
+    {
+      id: 3,
+      group: 1,
+      title: "item 3",
+      start_time: moment().add(2, "hour"),
+      end_time: moment().add(3, "hour"),
+    },
+  ];
+
+  var _stopSong = false;
+  var paletteNums = ["drums", "piano", "bass", "guitar"];
+  var paletteNumsStates = [1, 1, 1, 1];
+
+  function ToneButton0() {
+    const player = new Tone.Player("piano_middle_C.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButton1() {
+    const player = new Tone.Player("piano_C_sharp.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButton2() {
+    const player = new Tone.Player("piano_D.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButton3() {
+    const player = new Tone.Player("piano_D_sharp.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+  
+  function ToneButton4() {
+    const player = new Tone.Player("piano_E.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButtonN1() {
+    const player = new Tone.Player("kick.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButtonN2() {
+    const player = new Tone.Player("highhat.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButtonN3() {
+    const player = new Tone.Player("snare.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function ToneButtonN4() {
+    const player = new Tone.Player("clap.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function RecordButton() {
+    const player = new Tone.Player("metronome.mp3").toDestination();
+    Tone.loaded().then(() => {
+      player.start();
+    });
+  }
+
+  function Drum1() {
+    paletteNumsStates[0] = 1;
+    const player = new Tone.Player(
+      "drums" + paletteNumsStates[0] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Drum2() {
+    paletteNumsStates[0] = 2;
+    const player = new Tone.Player(
+      "drums" + paletteNumsStates[0] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Piano1() {
+    paletteNumsStates[1] = 1;
+    const player = new Tone.Player(
+      "piano" + paletteNumsStates[1] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Piano2() {
+    paletteNumsStates[1] = 2;
+    const player = new Tone.Player(
+      "piano" + paletteNumsStates[1] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Bass1() {
+    paletteNumsStates[2] = 1;
+    const player = new Tone.Player(
+      "bass" + paletteNumsStates[2] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Bass2() {
+    paletteNumsStates[2] = 2;
+    const player = new Tone.Player(
+      "bass" + paletteNumsStates[2] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Guitar1() {
+    paletteNumsStates[3] = 1;
+    const player = new Tone.Player(
+      "guitar" + paletteNumsStates[3] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function Guitar2() {
+    paletteNumsStates[3] = 2;
+    const player = new Tone.Player(
+      "guitar" + paletteNumsStates[3] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player.start();
+      });
+    }
+  }
+
+  function PlaySong() {
+    var player1 = new Tone.Player(
+      "drums" + paletteNumsStates[0] + ".mp3"
+    ).toDestination();
+    var player2 = new Tone.Player(
+      "piano" + paletteNumsStates[1] + ".mp3"
+    ).toDestination();
+    var player3 = new Tone.Player(
+      "bass" + paletteNumsStates[2] + ".mp3"
+    ).toDestination();
+    var player4 = new Tone.Player(
+      "guitar" + paletteNumsStates[3] + ".mp3"
+    ).toDestination();
+    if (_stopSong == false) {
+      Tone.loaded().then(() => {
+        player1.start();
+        player2.start();
+        player3.start();
+        player4.start();
+      });
+    }
+  }
+
+  function StopSong() {
+    if (_stopSong == false) {
+      _stopSong = true;
+    } else {
+      _stopSong = false;
+    }
+  }
 
   // put the user in their web socket room (room # = session ID)
-//   const startSession = async () => {
-//     const newSocket = io("http://localhost:5000/api");
-//     setSocket(newSocket);
-//     const member = await getCurrentMember();
-//     const data = {
-//       fullName: member.firstname + " " + member.lastname,
-//       sessionId: sessionId,
-//     };
-//     socket.emit("join", data);
-//   };
+  //   const startSession = async () => {
+  //     const newSocket = io("http://localhost:5000/api");
+  //     setSocket(newSocket);
+  //     const member = await getCurrentMember();
+  //     const data = {
+  //       fullName: member.firstname + " " + member.lastname,
+  //       sessionId: sessionId,
+  //     };
+  //     socket.emit("join", data);
+  //   };
 
   useEffect(() => {
+    // var player = new Tone.Player("drums" + paletteNumsStates[0] + ".mp3").toDestination();
     // startSession();
+    var player = new Tone.Player(
+      "drums" + paletteNumsStates[0] + ".mp3"
+    ).toDestination();
   }, []);
 
   const addLayer = () => {
@@ -95,6 +302,207 @@ function Session() {
 
   return (
     <Page>
+      {/* <div id="PaletteList" className="palette-list">
+          <a href="#" onClick={ShowPalette}>Rock</a>
+          <a href="#" onClick={ShowPalette}>Electronic</a>
+          <a href="#" onClick={ShowPalette}>Lo-Fi</a>
+          <a href="#" onClick={ShowPalette}>Hip-Hop</a>
+        </div> */}
+
+      <br></br>
+
+      <table className="table-palette">
+        <thead>
+          <tr>
+            <th>Palette:</th>
+            <th className="table-palette-th2">ROCK</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <td>
+              <button onClick={PlaySong}>Play</button>
+            </td>
+            <td>
+              <button onClick={StopSong}>STOP</button>
+            </td>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Drum1}
+                >
+                  Drums1
+                </button>
+              </div>
+            </td>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Drum2}
+                >
+                  Drums2
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Piano1}
+                >
+                  Piano1
+                </button>
+              </div>
+            </td>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Piano2}
+                >
+                  Piano2
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Bass1}
+                >
+                  Bass1
+                </button>
+              </div>
+            </td>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Bass2}
+                >
+                  Bass2
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Guitar1}
+                >
+                  Guitar1
+                </button>
+              </div>
+            </td>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button
+                  className="button-palette"
+                  role="button"
+                  onClick={Guitar2}
+                >
+                  Guitar2
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br></br>
+      <button onClick={RecordButton}>Record</button>
+      <br></br>
+      <button onClick={ToneButton0}>C</button>
+      <button onClick={ToneButton1}>C#</button>
+      <button onClick={ToneButton2}>D</button>
+      <button onClick={ToneButton3}>D#</button>
+      <button onClick={ToneButton4}>E</button>
+      <br></br>
+      <br></br>
+      <button onClick={ToneButtonN1}>Kick</button>
+      <button onClick={ToneButtonN2}>High Hat</button>
+      <button onClick={ToneButtonN3}>Snare</button>
+      <button onClick={ToneButtonN4}>Clap</button>
+
+      <br></br>
+      <button className="button-palette" role="button">
+        Piano
+      </button>
+
+      <br></br>
+      <br></br>
+
+      <table className="table-palette">
+        <thead>
+          <tr>
+            <th>Palette:</th>
+            <th className="table-palette-th2">ROCK</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <td>
+              <button>RESET</button>
+            </td>
+            <td>
+              <button>SAVE</button>
+            </td>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button className="button-palette" role="button">
+                  Piano
+                </button>
+              </div>
+            </td>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button className="button-palette" role="button">
+                  Overdriven Guitar
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button className="button-palette" role="button">
+                  Rock Drums
+                </button>
+              </div>
+            </td>
+            <td>
+              <div className="table-palette-buttonframe">
+                <button className="button-palette" role="button">
+                  Bass Guitar
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       {/* <Text h1>Session Page</Text>
       <Grid.Container gap={1} justify="flex-end" height="100px">
         <audio controls id="">
@@ -105,7 +513,7 @@ function Session() {
         </audio>
       </Grid.Container> */}
       <Head>
-          <h3>Your song session! Add a layer to your song with your partner!</h3>
+        <h3>Your song session! Add a layer to your song with your partner!</h3>
       </Head>
       <div>
         <Timeline
