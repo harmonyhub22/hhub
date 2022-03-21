321f49ce251404987234fffd1ea411397e497f35
Vi Truong <vitruong00@tamu.edu>
2022-03-21 17:17:54 -0500

    Testing

---
+++ components/sessions/SessionUI.tsx
@@ -0,0 +1,38 @@
+import { AnyMxRecord } from "dns";
+import React, { Component } from "react";
+import Layer from "../../interfaces/models/Layer";
+import Session from "../../interfaces/models/Session";
+import SessionData from "../../interfaces/session_data";
+import LayersCreated from "../../interfaces/socket-data/layers_created";
+import Member from "../../interfaces/models/Member";
+
+interface SessionUIProps {}
+
+interface SessionUIState {
+  layers: Layer;
+  session: Session;
+  sessionData: SessionData;
+  partner: Member;
+}
+class SessionUI extends Component<SessionUIProps, SessionUIState> {
+  constructor(props: SessionUIProps) {
+    super(props);
+    // this.state = {
+
+    // };
+  }
+  partnerLayer(layerId: string) {
+    if (session?.sessionId === undefined) return;
+    const layer: Layer | null = await getLayerById(session?.sessionId, layerId);
+    if (layer === null || layer === undefined) {
+      console.log("could not get partner layer");
+      return;
+    }
+    setLayers([...layers, layer]);
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SessionUI;
--- pages/sessions/[sid].tsx
+++ pages/sessions/[sid].tsx
@@ -1,12 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useRouter } from "next/router";
-import {
-  Button,
-  Page,
-  Drawer,
-  Modal,
-  useModal,
-} from "@geist-ui/core";
+import { Button, Page, Drawer, Modal, useModal } from "@geist-ui/core";
 import Head from "next/head";
 import Layer from "../../interfaces/models/Layer";
 import { MemberContext } from "../../context/member";
@@ -15,7 +9,12 @@ import Crunker from "crunker";
 import Palette from "../../components/ui/Palette";
 import Session from "../../interfaces/models/Session";
 import PaletteData from "../../interfaces/palette_data";
-import { getLayerById, getSession, postLayer, postLayerRecording } from "../../components/Session";
+import {
+  getLayerById,
+  getSession,
+  postLayer,
+  postLayerRecording,
+} from "../../components/Session";
 import SessionData from "../../interfaces/session_data";
 import LayerCreated from "../../interfaces/socket-data/layers_created";
 import LayersCreated from "../../interfaces/socket-data/layers_created";
@@ -24,7 +23,6 @@ import Member from "../../interfaces/models/Member";
 function Session() {
   const { visible, setVisible, bindings } = useModal();
   const [showPallete, setShowPalette] = React.useState(false);
-
 
   const [session, setSession] = useState<Session>();
   const [sessionData, setSessionData] = useState<SessionData>({
@@ -38,25 +36,25 @@ function Session() {
   const member = useContext(MemberContext);
   const socket = useContext(SocketContext);
 
-  const partnerLayer = async (layerId:string) => {
+  const partnerLayer = async (layerId: string) => {
     if (session?.sessionId === undefined) return;
     const layer: Layer | null = await getLayerById(session?.sessionId, layerId);
     if (layer === null || layer === undefined) {
-      console.log('could not get partner layer');
-      return; 
+      console.log("could not get partner layer");
+      return;
     }
     setLayers([...layers, layer]);
   };
 
   const getThisSession = async () => {
     console.log(router.query.sid);
-    const s = await getSession(router?.query?.sid?.toString() || '');
+    const s = await getSession(router?.query?.sid?.toString() || "");
     console.log(s);
     if (s === null || s === undefined) return;
     setSession(s);
     setLayers(s.layers); // set the current layers
     setPartner(s.member1.memberId === member.memberId ? s.member2 : s.member1); // set the partner
-    socket.on("layers_added", async (data:LayersCreated) => {
+    socket.on("layers_added", async (data: LayersCreated) => {
       data.layerIds.map(async (layerId) => {
         await partnerLayer(layerId);
       });
@@ -65,89 +63,89 @@ function Session() {
 
   useEffect(() => {
     getThisSession();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const addLayer = async (paletteData:PaletteData, selectedPatterns:string[]) => {
-    if (paletteData === null || paletteData === undefined)
-      return;
-    if (!paletteData.name) {
-      alert("Please seclect a palette pattern to use!");
-      return;
-    }
-    if (session?.sessionId === undefined) return;
+  // const addLayer = async (paletteData:PaletteData, selectedPatterns:string[]) => {
+  //   if (paletteData === null || paletteData === undefined)
+  //     return;
+  //   if (!paletteData.name) {
+  //     alert("Please seclect a palette pattern to use!");
+  //     return;
+  //   }
+  //   if (session?.sessionId === undefined) return;
 
-    // each of Will's patterns is 4 measures = .7 seconds
-    // therefore, there are .7/4 = .175 seconds per measure
-    const startTime = paletteData.startMeasure * 0.175;
-    const endTime = startTime + 0.7 * (paletteData.numRepeats + 1);
-    const layerData = {
-      startTime: startTime,
-      endTime: endTime,
-      repeatCount: paletteData.numRepeats,
-      file: '',
-    };
+  //   // each of Will's patterns is 4 measures = .7 seconds
+  //   // therefore, there are .7/4 = .175 seconds per measure
+  //   const startTime = paletteData.startMeasure * 0.175;
+  //   const endTime = startTime + 0.7 * (paletteData.numRepeats + 1);
+  //   const layerData = {
+  //     startTime: startTime,
+  //     endTime: endTime,
+  //     repeatCount: paletteData.numRepeats,
+  //     file: '',
+  //   };
 
-    const newLayers: Layer[] = [];
-    selectedPatterns.forEach(async (patternName:string) => {
+  //   const newLayers: Layer[] = [];
+  //   selectedPatterns.forEach(async (patternName:string) => {
 
-      layerData.file = patternName;
+  //     layerData.file = patternName;
 
-      const newLayer: Layer | null = await postLayer(session.sessionId, layerData);
-      if (newLayer === null) {
-        console.log('could not add layer');
-        alert('could not add layer');
-        return;
-      }
-      console.log(newLayer);
+  //     const newLayer: Layer | null = await postLayer(session.sessionId, layerData);
+  //     if (newLayer === null) {
+  //       console.log('could not add layer');
+  //       alert('could not add layer');
+  //       return;
+  //     }
+  //     console.log(newLayer);
 
-      newLayers.push(newLayer);
-    });
+  //     newLayers.push(newLayer);
+  //   });
 
-    setLayers([...layers, ...newLayers]);
+  //   setLayers([...layers, ...newLayers]);
 
-    // adjust the measures needed
-    const measuresNeeded = paletteData.startMeasure + 4 * (paletteData.numRepeats + 1);
-    if (measuresNeeded > paletteData.maxMeasuresNeeded) {
-      sessionData.measures = measuresNeeded;
-      setSessionData(sessionData);
-    }
+  //   // adjust the measures needed
+  //   const measuresNeeded = paletteData.startMeasure + 4 * (paletteData.numRepeats + 1);
+  //   if (measuresNeeded > paletteData.maxMeasuresNeeded) {
+  //     sessionData.measures = measuresNeeded;
+  //     setSessionData(sessionData);
+  //   }
 
-    // tell partner there's a new layer
-    const newLayerIds = newLayers.map((l) => l.layerId);
-    const dataToEmit = {
-      layerIds: newLayerIds,
-    };
-    socket.emit("add_layer", dataToEmit);
-  };
+  //   // tell partner there's a new layer
+  //   const newLayerIds = newLayers.map((l) => l.layerId);
+  //   const dataToEmit = {
+  //     layerIds: newLayerIds,
+  //   };
+  //   socket.emit("add_layer", dataToEmit);
+  // };
 
   // the left offset and width of the layer depends on the start time and ratio of layer duration to the entire song, respectively
-  const computeLayerStyle = (start:number, duration:number) => {
-      const layerStyle = {
-        width: "50px",
-        marginLeft: "0px",
-      };
+  const computeLayerStyle = (start: number, duration: number) => {
+    const layerStyle = {
+      width: "50px",
+      marginLeft: "0px",
+    };
 
-      let durationPerentage = duration / (sessionData.measures * .175) * 100;
-      layerStyle.width = durationPerentage.toString() + "%";
-      
-      // the percentage of left offset decreases as the # of columns in our table increases (since columns shrink as more are added)
-      let multiplier = 20;
-      if (sessionData.measures >= 5 && sessionData.measures < 10) multiplier = 10;
-      else if (sessionData.measures >= 10) multiplier = 5;
-      const leftOffset = start * multiplier;
-      layerStyle.marginLeft = leftOffset.toString() + "%";
-      return layerStyle;
+    let durationPerentage = (duration / (sessionData.measures * 0.175)) * 100;
+    layerStyle.width = durationPerentage.toString() + "%";
+
+    // the percentage of left offset decreases as the # of columns in our table increases (since columns shrink as more are added)
+    let multiplier = 20;
+    if (sessionData.measures >= 5 && sessionData.measures < 10) multiplier = 10;
+    else if (sessionData.measures >= 10) multiplier = 5;
+    const leftOffset = start * multiplier;
+    layerStyle.marginLeft = leftOffset.toString() + "%";
+    return layerStyle;
   };
-  const loop = async (file:string,loopcount:number)=>{
-    const crunker = new Crunker();
-    const arr:AudioBuffer[] = []
-    for (var i = 0; i < loopcount; i++) {
-      const temp = await crunker.fetchAudio(file)
-      arr.push(temp[0])
-    }
-    return crunker.concatAudio(arr)
-  }
+  // const loop = async (file:string,loopcount:number)=>{
+  //   const crunker = new Crunker();
+  //   const arr:AudioBuffer[] = []
+  //   for (var i = 0; i < loopcount; i++) {
+  //     const temp = await crunker.fetchAudio(file)
+  //     arr.push(temp[0])
+  //   }
+  //   return crunker.concatAudio(arr)
+  // }
 
   const finishSong = async () => {
     // TODO: make backend request to process the finished song (send all of the layers)
@@ -155,14 +153,15 @@ function Session() {
     const buffers: AudioBuffer[] = [];
     const getBuffers = new Promise(() => {
       layers.map(async (layer) => {
-      const buffer = await crunker.fetchAudio(layer.file);
-      return crunker.padAudio(buffer[0], 0, layer.startTime);
-    })});
+        const buffer = await crunker.fetchAudio(layer.file);
+        return crunker.padAudio(buffer[0], 0, layer.startTime);
+      });
+    });
     await getBuffers;
 
-    const t = crunker.mergeAudio(buffers)
-    const output = crunker.export(t, 'audio/mp3')
-    crunker.download(output.blob)
+    const t = crunker.mergeAudio(buffers);
+    const output = crunker.export(t, "audio/mp3");
+    crunker.download(output.blob);
     socket.emit("finished");
   };
 
@@ -184,31 +183,46 @@ function Session() {
         <table id="session-timeline">
           <tbody>
             <tr>
-              {Array.from(Array(sessionData.measures), (_, i) => <th key={"M_" + i}>M{i}</th>)}
+              {Array.from(Array(sessionData.measures), (_, i) => (
+                <th key={"M_" + i}>M{i}</th>
+              ))}
             </tr>
-            {((layers?.length ?? 0) !== 0) ?
-              (layers.map((layer, i) => {
+            {(layers?.length ?? 0) !== 0 ? (
+              layers.map((layer, i) => {
                 return (
                   <tr key={"layer_" + i}>
                     <td colSpan={sessionData.measures}>
                       <audio
                         controls
-                        style={computeLayerStyle(layer.startTime, layer.endTime - layer.startTime)}
-                        src={((layer?.file?.length ?? 0)=== 0) ? layer.file : layer.bucketUrl}
+                        style={computeLayerStyle(
+                          layer.startTime,
+                          layer.endTime - layer.startTime
+                        )}
+                        src={
+                          (layer?.file?.length ?? 0) === 0
+                            ? layer.file
+                            : layer.bucketUrl
+                        }
                       ></audio>
-                      {layer.memberId === member.memberId ? 
-                      (member.firstname[0].toUpperCase() + member.lastname[0].toUpperCase()) :
-                      (partner !== undefined && partner.firstname[0].toUpperCase() + partner.lastname[0].toUpperCase())}
+                      {layer.memberId === member.memberId
+                        ? member.firstname[0].toUpperCase() +
+                          member.lastname[0].toUpperCase()
+                        : partner !== undefined &&
+                          partner.firstname[0].toUpperCase() +
+                            partner.lastname[0].toUpperCase()}
                     </td>
                   </tr>
-                ) 
-                })) :
-              (<tr>
+                );
+              })
+            ) : (
+              <tr>
                 <td key={"first_layer"}>
-                  <Button onClick={() => setShowPalette(true)}>Create your first layer!</Button>
+                  <Button onClick={() => setShowPalette(true)}>
+                    Create your first layer!
+                  </Button>
                 </td>
-              </tr>)
-            }
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
@@ -219,7 +233,10 @@ function Session() {
         placement="right"
       >
         <Drawer.Content>
-          <Palette genreName={"alt"} initials={`${member.firstname[0]}${member.lastname[0]}`} />
+          <Palette
+            genreName={"alt"}
+            initials={`${member.firstname[0]}${member.lastname[0]}`}
+          />
         </Drawer.Content>
       </Drawer>
 
