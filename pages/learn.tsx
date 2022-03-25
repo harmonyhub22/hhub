524553ece0f52fdec4a3ae4fda40e3576b07fab1
Dean Orenstein <dto3576@gmail.com>
2022-03-25 18:31:01 -0500

    start music theory page

--- pages/learn.tsx
+++ pages/learn.tsx
@@ -1,13 +1,35 @@
 import { useRouter } from "next/router";
 import Navbar from "../components/Navbar";
+import { Page, Text, Divider, Link } from "@geist-ui/core";
 
 const Learn = () => {
   const Router = useRouter();
   return (
-    <>
+    <Page>
       <Navbar />
-      <h1>Learn Music Theory</h1>
-    </>
+      <Text h1>Learn Music Theory</Text>
+      <Text>Looking to learn what things mean? We got your back!</Text>
+
+      <Text h3>Tempo</Text>
+      <Text>
+        Tempo is the underlying beat of the song. This is what keeps your song together so that
+        it has structure. It is what plays in the background as you add to your song.
+      </Text>
+      <Link href="#" icon color>Learn more</Link>
+      <Divider /> {/* --------------------------------------------------------------------- */}
+      <Text h3>Time Signature</Text>
+      <Text>
+        
+      </Text>
+      <Link href="#" icon color>Learn more</Link>
+      <Divider /> {/* --------------------------------------------------------------------- */}
+      <Text h3>Rhythm</Text>
+      <Text>
+        
+      </Text>
+      <Link href="#" icon color>Learn more</Link>
+      <Divider /> {/* --------------------------------------------------------------------- */}
+    </Page>
   );
 };
 
