7420ca7e88067d55b18fcb9a055b9669cad344ed
Olumayowa Olowomeye <52811635+Mav-ert@users.noreply.github.com>
2022-04-13 08:21:55 -0500

    youtube stuff

--- pages/tutorial.tsx
+++ pages/tutorial.tsx
@@ -12,7 +12,9 @@ const Tutorial = () => {
           <Text h1>Tutorial</Text>
           <Text h4>How to use Harmony Hub!</Text>
         </div>
-        <iframe width="720" height="520" src="https://youtube.com/embed/JBqOLxqZ4L8"></iframe>
+        <div className="video" style={{position: "relative",paddingBottom: "56.25%",paddingTop: 25,height: 0, justifyContent: "center"}}>
+          <iframe style={{position: "absolute",top: 0,left: 0,width: "80%",height: "80%"}} src={`https://www.youtube.com/embed/JBqOLxqZ4L8`}frameBorder="0"/>
+        </div>
       </Page>
     </>
   );
