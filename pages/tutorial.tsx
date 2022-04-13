cc0f5a959001cb67e4b502988c8adf076f0cd7cb
Dean Orenstein <dto3576@gmail.com>
2022-04-13 08:12:11 -0500

    tutorial vid

--- pages/tutorial.tsx
+++ pages/tutorial.tsx
@@ -7,11 +7,12 @@ const Tutorial = () => {
   return (
     <>
       <Navbar />
-      <Page>
+      <Page className="tutorial">
         <div style={{display: 'flex', width: '100%', justifyContent: "center", textAlign: 'center', flexDirection: 'column'}}>
           <Text h1>Tutorial</Text>
           <Text h4>How to use Harmony Hub!</Text>
         </div>
+        <iframe width="720" height="520" src="https://youtube.com/embed/JBqOLxqZ4L8"></iframe>
       </Page>
     </>
   );
